5e1d0b6c944556c373a98b89aa637645150ccc55
Arina Umushkina <umushkina.arina@yandex.ru>
2021-03-11 14:48:23 +0300

    add login generator

--- src/HW/Main.java
+++ src/HW/Main.java
@@ -2,6 +2,8 @@ package HW;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("HW");
+
+    LoginGenerator loginGenerator = new LoginGenerator();
+    System.out.println(loginGenerator.getLogin());
   }
 }
